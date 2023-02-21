12fa3eecea2b9af2e70a97c838207e447831db27
Brenno Oliveira <120618226+Brenno0liveira@users.noreply.github.com>
2023-02-20 21:41:44 -0300

    Add files via upload

---
+++ Carômetro/src/view/Carometro.java
@@ -0,0 +1,49 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class Carometro extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Carometro frame = new Carometro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Carometro() {
+		setTitle("Carômetro");
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Carometro.class.getResource("/img/avatar.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 360);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
